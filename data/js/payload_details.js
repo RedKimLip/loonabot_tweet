477252f07395524bfb968191ac4324cc086ecffc
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2019-05-02 00:15:22 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4374,
-	"created_at": "Tue Apr 30 15:15:19 +0000 2019",
+	"tweets": 4376,
+	"created_at": "Wed May 01 15:15:21 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,416 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 03:20:34 +0000 2019",
+		"id": 1123421824690208800,
+		"id_str": "1123426962444300288",
+		"full_text": "[이달의소녀킼킼 #100]\n👩‍👧여슬자매 놀이동산 왔지😆😍\n\n#이달의소녀 #LOONA #LOONAKick #하슬 #여진 #HaSeul #YeoJin https://t.co/5GG8HQWLgC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						34,
+						40
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						41,
+						47
+					]
+				},
+				{
+					"text": "LOONAKick",
+					"indices": [
+						48,
+						58
+					]
+				},
+				{
+					"text": "하슬",
+					"indices": [
+						59,
+						62
+					]
+				},
+				{
+					"text": "여진",
+					"indices": [
+						63,
+						66
+					]
+				},
+				{
+					"text": "HaSeul",
+					"indices": [
+						67,
+						74
+					]
+				},
+				{
+					"text": "YeoJin",
+					"indices": [
+						75,
+						82
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123407269750759400,
+					"id_str": "1123407269750759424",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1123407269750759424/pu/img/6Qle4n9eCyAWfFlP.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1123407269750759424/pu/img/6Qle4n9eCyAWfFlP.jpg",
+					"url": "https://t.co/5GG8HQWLgC",
+					"display_url": "pic.twitter.com/5GG8HQWLgC",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1123421824690208768/video/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 360,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 360,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 360,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123407269750759400,
+					"id_str": "1123407269750759424",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1123407269750759424/pu/img/6Qle4n9eCyAWfFlP.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1123407269750759424/pu/img/6Qle4n9eCyAWfFlP.jpg",
+					"url": "https://t.co/5GG8HQWLgC",
+					"display_url": "pic.twitter.com/5GG8HQWLgC",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1123421824690208768/video/1",
+					"type": "video",
+					"sizes": {
+						"medium": {
+							"w": 360,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 360,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 360,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							3,
+							4
+						],
+						"duration_millis": 34292,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1123407269750759424/pu/vid/360x480/WFe2VWE0noUQUf4j.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1123407269750759424/pu/pl/c4rdtreXrtvz2N1a.m3u8?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1123407269750759424/pu/vid/320x426/S7IMQtvaOh7fYcod.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 293916,
+			"friends_count": 0,
+			"listed_count": 4015,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1460,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1550571087",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6321,
+		"favorite_count": 16266,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[이달의소녀킼킼 #100]\n👩‍👧여슬자매 놀이동산 왔지😆😍\n\n#이달의소녀 #LOONA #LOONAKick #하슬 #여진 #HaSeul #YeoJin https://t.co/5GG8HQWLgC"
+	},
+	{
+		"created_at": "Wed May 01 03:05:39 +0000 2019",
+		"id": 1123423207464341500,
+		"id_str": "1123423207464341504",
+		"full_text": "#LOONA_Instagram\nVideo: 💕 [이달의소녀킼킼 #100] 👩‍👧여슬자매 놀이동산 왔지😆😍 ⠀⠀⠀⠀⠀ 👩‍👧YeoSeul is here at the amusement park😆😍⠀ ⠀⠀⠀⠀⠀ #이달의소녀 #LOONA #LOONAKick #하슬 #여진 #HaSeul #YeoJin\nhttps://t.co/fcmls6Px7X",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA_Instagram",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						115,
+						121
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						122,
+						128
+					]
+				},
+				{
+					"text": "LOONAKick",
+					"indices": [
+						129,
+						139
+					]
+				},
+				{
+					"text": "하슬",
+					"indices": [
+						140,
+						143
+					]
+				},
+				{
+					"text": "여진",
+					"indices": [
+						144,
+						147
+					]
+				},
+				{
+					"text": "HaSeul",
+					"indices": [
+						148,
+						155
+					]
+				},
+				{
+					"text": "YeoJin",
+					"indices": [
+						156,
+						163
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fcmls6Px7X",
+					"expanded_url": "https://www.instagram.com/p/Bw5wz-ih9_-/",
+					"display_url": "instagram.com/p/Bw5wz-ih9_-/",
+					"indices": [
+						164,
+						187
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884869555343863800,
+			"id_str": "884869555343863808",
+			"name": "레드립",
+			"screen_name": "RedKimLip",
+			"location": "",
+			"description": "이달의 소녀(LOOΠΔ) 김립 💋\n@loonatheworld",
+			"url": "https://t.co/QynukFehbM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QynukFehbM",
+							"expanded_url": "http://t.me/loonatheworld",
+							"display_url": "t.me/loonatheworld",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 79,
+			"friends_count": 81,
+			"listed_count": 10,
+			"created_at": "Tue Jul 11 20:18:36 +0000 2017",
+			"favourites_count": 482,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4338,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/994166992247533570/3n3Z1oQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/994166992247533570/3n3Z1oQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884869555343863808/1499807258",
+			"profile_link_color": "D8193D",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#LOONA_Instagram\nVideo: 💕 [이달의소녀킼킼 #100] 👩‍👧여슬자매 놀이동산 왔지😆😍 ⠀⠀⠀⠀⠀ 👩‍👧YeoSeul is here at the amusement park😆😍⠀ ⠀⠀⠀⠀⠀ #이달의소녀 #LOONA #LOONAKick #하슬 #여진 #HaSeul #YeoJin\nhttps://t.co/fcmls6Px7X"
+	}
+]
