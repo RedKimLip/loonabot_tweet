a97da3c8368bd20853046fa719feddc9916eb17e
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2019-09-09 00:15:31 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4756,
-	"created_at": "Sat Sep 07 15:15:17 +0000 2019",
+	"tweets": 4757,
+	"created_at": "Sun Sep 08 15:15:30 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 08 03:47:58 +0000 2019",
+		"id": 1170413927747989500,
+		"id_str": "1170544280861138944",
+		"full_text": "\"me!! choose me!! i know this song!\" ft. @loonatheworld https://t.co/K4RNqoEuqq https://t.co/DKAx9Zlilz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonatheworld",
+					"name": "이달의 소녀(LOOΠΔ)",
+					"id": 770959712791597000,
+					"id_str": "770959712791597056",
+					"indices": [
+						41,
+						55
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/K4RNqoEuqq",
+					"expanded_url": "https://youtu.be/1S2KvOvSXFk",
+					"display_url": "youtu.be/1S2KvOvSXFk",
+					"indices": [
+						56,
+						79
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1170144358474997800,
+					"id_str": "1170144358474997760",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1170144358474997760/img/7X3ECPqqa3mCntNH.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1170144358474997760/img/7X3ECPqqa3mCntNH.jpg",
+					"url": "https://t.co/DKAx9Zlilz",
+					"display_url": "pic.twitter.com/DKAx9Zlilz",
+					"expanded_url": "https://twitter.com/FBE/status/1170413927747989504/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170144358474997800,
+					"id_str": "1170144358474997760",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1170144358474997760/img/7X3ECPqqa3mCntNH.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1170144358474997760/img/7X3ECPqqa3mCntNH.jpg",
+					"url": "https://t.co/DKAx9Zlilz",
+					"display_url": "pic.twitter.com/DKAx9Zlilz",
+					"expanded_url": "https://twitter.com/FBE/status/1170413927747989504/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 39991,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1170144358474997760/vid/480x270/oK4p-r2XcI4Lro8-.mp4?tag=13"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1170144358474997760/vid/1280x720/BKkrfITAzWFcoM3G.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1170144358474997760/vid/640x360/YqYDW3kyM_6QH4Iz.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1170144358474997760/pl/QOfW4Hu8ppc3ywzD.m3u8?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16547369,
+			"id_str": "16547369",
+			"name": "FBE",
+			"screen_name": "FBE",
+			"location": "Los Angeles, CA",
+			"description": "This is the official FBE Twitter. We make originals for web & TV. Join the conversation!",
+			"url": "https://t.co/wca5tMf2ld",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wca5tMf2ld",
+							"expanded_url": "https://youtube.com/fbe",
+							"display_url": "youtube.com/fbe",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 904474,
+			"friends_count": 1943,
+			"listed_count": 1746,
+			"created_at": "Wed Oct 01 18:04:28 +0000 2008",
+			"favourites_count": 6901,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 44829,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1132048001755537408/05Ors0jw_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1132048001755537408/05Ors0jw_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16547369/1558742401",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2962,
+		"favorite_count": 8129,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"me!! choose me!! i know this song!\" ft. @loonatheworld https://t.co/K4RNqoEuqq https://t.co/DKAx9Zlilz"
+	},
+	{
 		"created_at": "Sat Sep 07 11:09:35 +0000 2019",
 		"id": 1170293029762211800,
 		"id_str": "1170293029762211840",
