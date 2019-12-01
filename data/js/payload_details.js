c234da9c85ef16ca6475f65d9613eb21f9205bc1
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2019-12-01 09:38:53 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4939,
-	"created_at": "Fri Nov 29 22:38:53 +0000 2019",
+	"tweets": 4940,
+	"created_at": "Sun Dec 01 00:38:52 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,299 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 00:10:00 +0000 2019",
+		"id": 1200710577984688000,
+		"id_str": "1200930008446853120",
+		"full_text": "190924 에버랜드\n#이달의소녀 #오드아이써클 #진솔 #김립 #최리\n\n핫유닛오드아이써클 제발 컴백해줘🥺🥺 https://t.co/xuUE8Du7g7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						12,
+						18
+					]
+				},
+				{
+					"text": "오드아이써클",
+					"indices": [
+						19,
+						26
+					]
+				},
+				{
+					"text": "진솔",
+					"indices": [
+						27,
+						30
+					]
+				},
+				{
+					"text": "김립",
+					"indices": [
+						31,
+						34
+					]
+				},
+				{
+					"text": "최리",
+					"indices": [
+						35,
+						38
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200710567503155200,
+					"id_str": "1200710567503155200",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EKnH5kYVUAA-iEi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKnH5kYVUAA-iEi.jpg",
+					"url": "https://t.co/xuUE8Du7g7",
+					"display_url": "pic.twitter.com/xuUE8Du7g7",
+					"expanded_url": "https://twitter.com/seomalja_/status/1200710577984688128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 774,
+									"y": 256,
+									"h": 324,
+									"w": 324
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 453,
+									"y": 150,
+									"h": 189,
+									"w": 189
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 774,
+									"y": 256,
+									"h": 324,
+									"w": 324
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 256,
+									"y": 85,
+									"h": 107,
+									"w": 107
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200710567503155200,
+					"id_str": "1200710567503155200",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/EKnH5kYVUAA-iEi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKnH5kYVUAA-iEi.jpg",
+					"url": "https://t.co/xuUE8Du7g7",
+					"display_url": "pic.twitter.com/xuUE8Du7g7",
+					"expanded_url": "https://twitter.com/seomalja_/status/1200710577984688128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 774,
+									"y": 256,
+									"h": 324,
+									"w": 324
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 453,
+									"y": 150,
+									"h": 189,
+									"w": 189
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 774,
+									"y": 256,
+									"h": 324,
+									"w": 324
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 256,
+									"y": 85,
+									"h": 107,
+									"w": 107
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1101028043961712600,
+			"id_str": "1101028043961712640",
+			"name": "🦉서말자🦉",
+			"screen_name": "seomalja_",
+			"location": "🐰🐱🕊🐸🦌🦉🐟🦇🦢🐧🦋🐺",
+			"description": "립아 도망갈까? ❌RPS❌ 비팔알림 🔕 사진은 모멘트",
+			"url": "https://t.co/DnxLkBA2l0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DnxLkBA2l0",
+							"expanded_url": "https://peing.net/seomalja_",
+							"display_url": "peing.net/seomalja_",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 590,
+			"friends_count": 125,
+			"listed_count": 41,
+			"created_at": "Thu Feb 28 07:55:16 +0000 2019",
+			"favourites_count": 2924,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10920,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1200731167021944832/uHGNOp7o_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1200731167021944832/uHGNOp7o_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1101028043961712640/1575111594",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 218,
+		"favorite_count": 437,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "190924 에버랜드\n#이달의소녀 #오드아이써클 #진솔 #김립 #최리\n\n핫유닛오드아이써클 제발 컴백해줘🥺🥺 https://t.co/xuUE8Du7g7"
+	}
+]
