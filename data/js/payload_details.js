50199ec598de5a3872c0e32d18a8df14bc762c5f
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2019-12-27 01:04:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5006,
-	"created_at": "Thu Dec 26 15:04:24 +0000 2019",
+	"tweets": 5007,
+	"created_at": "Thu Dec 26 16:04:25 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 26 15:43:20 +0000 2019",
+		"id": 1210224583531348000,
+		"id_str": "1210224583531347968",
+		"full_text": "#오빛트친소 리스트테스트",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "오빛트친소",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1027943210419548200,
+			"id_str": "1027943210419548161",
+			"name": "레드립 💋",
+			"screen_name": "redkimlip",
+			"location": "",
+			"description": "#이달의소녀 #LOONA @loonatheworld \n#김립 #KimLip\n\n📧 E-mail : redkimlip@gmail.com\n\n이달의 소녀 봇 @loonabot 관리자",
+			"url": "https://t.co/QynukFehbM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QynukFehbM",
+							"expanded_url": "http://t.me/loonatheworld",
+							"display_url": "t.me/loonatheworld",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1,
+			"friends_count": 120,
+			"listed_count": 1,
+			"created_at": "Fri Aug 10 15:42:14 +0000 2018",
+			"favourites_count": 25,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 84,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1195643754020888577/05vapBWA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1195643754020888577/05vapBWA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1027943210419548161/1573886512",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": true,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "#오빛트친소 리스트테스트"
+	},
+	{
 		"created_at": "Thu Dec 26 14:45:37 +0000 2019",
 		"id": 1210210061223485400,
 		"id_str": "1210210061223485441",
