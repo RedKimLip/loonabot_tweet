4f65f21ddc25c3e3bbb519a3847e06ecacd9d966
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-03-14 02:04:27 +0900

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 13 16:10:20 +0000 2020",
+		"id": 1238347834304295000,
+		"id_str": "1238497632416198656",
+		"full_text": "나를 좋아하는 사람들은 애 좋아할까?\n- 으음,,, 귀여워서..? 귀엽게에~~ https://t.co/K0thj8sHTW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1238347622168940500,
+					"id_str": "1238347622168940544",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1238347622168940544/pu/img/v74LXfQXNNEVAdlW.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1238347622168940544/pu/img/v74LXfQXNNEVAdlW.jpg",
+					"url": "https://t.co/K0thj8sHTW",
+					"display_url": "pic.twitter.com/K0thj8sHTW",
+					"expanded_url": "https://twitter.com/zmfflq1234/status/1238347834304294912/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 384,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1040,
+							"h": 588,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1040,
+							"h": 588,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1238347622168940500,
+					"id_str": "1238347622168940544",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1238347622168940544/pu/img/v74LXfQXNNEVAdlW.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1238347622168940544/pu/img/v74LXfQXNNEVAdlW.jpg",
+					"url": "https://t.co/K0thj8sHTW",
+					"display_url": "pic.twitter.com/K0thj8sHTW",
+					"expanded_url": "https://twitter.com/zmfflq1234/status/1238347834304294912/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 384,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1040,
+							"h": 588,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1040,
+							"h": 588,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							260,
+							147
+						],
+						"duration_millis": 39234,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1238347622168940544/pu/vid/636x360/mM0_Wbq_9WDDi8ei.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1238347622168940544/pu/pl/l_2N9_RI149l50B8.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1238347622168940544/pu/vid/1040x588/n2GNgWkKvwc965SN.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1238347622168940544/pu/vid/476x270/GQwVaRLtCSEpsYaE.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1180291928031514600,
+			"id_str": "1180291928031514624",
+			"name": "여자영상저장소",
+			"screen_name": "zmfflq1234",
+			"location": "여자만/모멘트에 정리",
+			"description": "내가 돌려보고싶은 영상 /@zmfflq12345",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61376,
+			"friends_count": 1,
+			"listed_count": 140,
+			"created_at": "Sat Oct 05 01:21:50 +0000 2019",
+			"favourites_count": 21,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1180291928031514624/1580997391",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": true,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2182,
+		"favorite_count": 1512,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "나를 좋아하는 사람들은 애 좋아할까?\n- 으음,,, 귀여워서..? 귀엽게에~~ https://t.co/K0thj8sHTW"
+	},
+	{
 		"created_at": "Fri Mar 13 05:27:48 +0000 2020",
 		"id": 1238320260517748700,
 		"id_str": "1238335933000253440",
