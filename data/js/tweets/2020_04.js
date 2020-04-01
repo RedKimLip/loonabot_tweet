8ba177c4441e382ea4cfe25c70de3c6186aafda7
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-04-01 16:04:25 +0900

    Added 2 tweets

---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,502 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 06:54:35 +0000 2020",
+		"id": 1245231606836547600,
+		"id_str": "1245243142334373891",
+		"full_text": "171104\n사랑 많이 주고 잘 챙겨주세요. 저도 정은이 한테 사랑과 행복을 세상에서 다 주고싶네요.\n@loonatheworld\n#김립 #KIMLIP #이달의소녀 #LOONA https://t.co/De51JdwrxF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김립",
+					"indices": [
+						72,
+						75
+					]
+				},
+				{
+					"text": "KIMLIP",
+					"indices": [
+						76,
+						83
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						84,
+						90
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						91,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonatheworld",
+					"name": "이달의 소녀(LOOΠΔ)",
+					"id": 770959712791597000,
+					"id_str": "770959712791597056",
+					"indices": [
+						57,
+						71
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245231333778976800,
+					"id_str": "1245231333778976768",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EUfzTPEU8AAsoW1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUfzTPEU8AAsoW1.jpg",
+					"url": "https://t.co/De51JdwrxF",
+					"display_url": "pic.twitter.com/De51JdwrxF",
+					"expanded_url": "https://twitter.com/whatloveis0210/status/1245231606836547586/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245231333778976800,
+					"id_str": "1245231333778976768",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EUfzTPEU8AAsoW1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUfzTPEU8AAsoW1.jpg",
+					"url": "https://t.co/De51JdwrxF",
+					"display_url": "pic.twitter.com/De51JdwrxF",
+					"expanded_url": "https://twitter.com/whatloveis0210/status/1245231606836547586/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1245231349952180200,
+					"id_str": "1245231349952180224",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EUfzULUUcAAcwhc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUfzULUUcAAcwhc.jpg",
+					"url": "https://t.co/De51JdwrxF",
+					"display_url": "pic.twitter.com/De51JdwrxF",
+					"expanded_url": "https://twitter.com/whatloveis0210/status/1245231606836547586/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1245231370579767300,
+					"id_str": "1245231370579767297",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EUfzVYKUcAEV6oA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUfzVYKUcAEV6oA.jpg",
+					"url": "https://t.co/De51JdwrxF",
+					"display_url": "pic.twitter.com/De51JdwrxF",
+					"expanded_url": "https://twitter.com/whatloveis0210/status/1245231606836547586/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1350,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1245231370772738000,
+					"id_str": "1245231370772738048",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EUfzVY4U8AAUW3P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUfzVY4U8AAUW3P.jpg",
+					"url": "https://t.co/De51JdwrxF",
+					"display_url": "pic.twitter.com/De51JdwrxF",
+					"expanded_url": "https://twitter.com/whatloveis0210/status/1245231606836547586/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 900,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1206877545313890300,
+			"id_str": "1206877545313890304",
+			"name": "what love is",
+			"screen_name": "whatloveis0210",
+			"location": "",
+			"description": "For LOONA Kim Lip\n❌Edit /❌Crop Logo /⭕️Crop for profile /⭕️Fanart\n#김립 #KIMLIP #이달의소녀 #LOONA",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4335,
+			"friends_count": 3,
+			"listed_count": 471,
+			"created_at": "Tue Dec 17 10:04:17 +0000 2019",
+			"favourites_count": 679,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 215,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234268039723372544/zH2Kh3pb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234268039723372544/zH2Kh3pb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1206877545313890304/1580960922",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 262,
+		"favorite_count": 584,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "171104\n사랑 많이 주고 잘 챙겨주세요. 저도 정은이 한테 사랑과 행복을 세상에서 다 주고싶네요.\n@loonatheworld\n#김립 #KIMLIP #이달의소녀 #LOONA https://t.co/De51JdwrxF"
+	},
+	{
+		"created_at": "Wed Apr 01 06:54:02 +0000 2020",
+		"id": 1245024127343190000,
+		"id_str": "1245243004148801538",
+		"full_text": "#희진 #이달의소녀 #LOOΠΔ \n#AprilFoolsDay https://t.co/6gxuMZfCDe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "희진",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						4,
+						10
+					]
+				},
+				{
+					"text": "LOOΠΔ",
+					"indices": [
+						11,
+						17
+					]
+				},
+				{
+					"text": "AprilFoolsDay",
+					"indices": [
+						19,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245024118124118000,
+					"id_str": "1245024118124118017",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EUc21smVAAEikeI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUc21smVAAEikeI.jpg",
+					"url": "https://t.co/6gxuMZfCDe",
+					"display_url": "pic.twitter.com/6gxuMZfCDe",
+					"expanded_url": "https://twitter.com/bxux_s/status/1245024127343190016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245024118124118000,
+					"id_str": "1245024118124118017",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EUc21smVAAEikeI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUc21smVAAEikeI.jpg",
+					"url": "https://t.co/6gxuMZfCDe",
+					"display_url": "pic.twitter.com/6gxuMZfCDe",
+					"expanded_url": "https://twitter.com/bxux_s/status/1245024127343190016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 799,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1207855972191850500,
+			"id_str": "1207855972191850497",
+			"name": "blue",
+			"screen_name": "bxux_s",
+			"location": "",
+			"description": "앙뇽 얘두다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21,
+			"friends_count": 10,
+			"listed_count": 0,
+			"created_at": "Fri Dec 20 02:51:33 +0000 2019",
+			"favourites_count": 325,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 181,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1207860136967979008/xG_1D6rb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207860136967979008/xG_1D6rb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1207855972191850497/1576811152",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 411,
+		"favorite_count": 768,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#희진 #이달의소녀 #LOOΠΔ \n#AprilFoolsDay https://t.co/6gxuMZfCDe"
+	}
+]
