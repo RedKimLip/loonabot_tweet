ad0ca348bc584e48e414d1f819a0a9b97c977c26
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-04-03 14:04:25 +0900

    Added 1 tweets

--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 03 04:20:49 +0000 2020",
+		"id": 1245680107970113500,
+		"id_str": "1245929222658355202",
+		"full_text": "190817 kcon \n\n#진솔 #JINSOUL🐟\n#이달의소녀 #LOONA https://t.co/FD5YluEa1i",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "진솔",
+					"indices": [
+						14,
+						17
+					]
+				},
+				{
+					"text": "JINSOUL",
+					"indices": [
+						18,
+						26
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						28,
+						34
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						35,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245678913210052600,
+					"id_str": "1245678913210052611",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/EUmKXyYUcAMpLP3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUmKXyYUcAMpLP3.jpg",
+					"url": "https://t.co/FD5YluEa1i",
+					"display_url": "pic.twitter.com/FD5YluEa1i",
+					"expanded_url": "https://twitter.com/trust_issues613/status/1245680107970113538/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245678913210052600,
+					"id_str": "1245678913210052611",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/EUmKXyYUcAMpLP3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUmKXyYUcAMpLP3.jpg",
+					"url": "https://t.co/FD5YluEa1i",
+					"display_url": "pic.twitter.com/FD5YluEa1i",
+					"expanded_url": "https://twitter.com/trust_issues613/status/1245680107970113538/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1245679641932644400,
+					"id_str": "1245679641932644354",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/EUmLCNFUwAIw0Xl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUmLCNFUwAIw0Xl.jpg",
+					"url": "https://t.co/FD5YluEa1i",
+					"display_url": "pic.twitter.com/FD5YluEa1i",
+					"expanded_url": "https://twitter.com/trust_issues613/status/1245680107970113538/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1245679655580885000,
+					"id_str": "1245679655580884993",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/EUmLC_7UYAExUsu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUmLC_7UYAExUsu.jpg",
+					"url": "https://t.co/FD5YluEa1i",
+					"display_url": "pic.twitter.com/FD5YluEa1i",
+					"expanded_url": "https://twitter.com/trust_issues613/status/1245680107970113538/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1084207845439094800,
+			"id_str": "1084207845439094784",
+			"name": "trust issues.",
+			"screen_name": "trust_issues613",
+			"location": "logo crop x, edit w credit o, profile crop o",
+			"description": "🐟",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22040,
+			"friends_count": 3,
+			"listed_count": 1557,
+			"created_at": "Sat Jan 12 21:57:48 +0000 2019",
+			"favourites_count": 262,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 655,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1245115032444100608/PrzI0Gl7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1245115032444100608/PrzI0Gl7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1084207845439094784/1547330999",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1488,
+		"favorite_count": 4357,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "tl",
+		"text": "190817 kcon \n\n#진솔 #JINSOUL🐟\n#이달의소녀 #LOONA https://t.co/FD5YluEa1i"
+	},
+	{
 		"created_at": "Wed Apr 01 11:00:34 +0000 2020",
 		"id": 1245048430608519200,
 		"id_str": "1245305046079647744",
