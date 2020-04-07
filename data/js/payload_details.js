300a4ee6cf6be51bc79c456332101cc43a95cf69
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-04-07 13:04:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5062,
-	"created_at": "Sun Apr 05 21:04:23 +0000 2020",
+	"tweets": 5063,
+	"created_at": "Tue Apr 07 04:04:24 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Tue Apr 07 03:09:29 +0000 2020",
+		"id": 1247200406662340600,
+		"id_str": "1247360823128817664",
+		"full_text": "191126 ✨\n\n#고원 #Gowon #이달의소녀 #LOONA https://t.co/zt6DrDVUVu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "고원",
+					"indices": [
+						10,
+						13
+					]
+				},
+				{
+					"text": "Gowon",
+					"indices": [
+						14,
+						20
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						21,
+						27
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						28,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1247200228630909000,
+					"id_str": "1247200228630908934",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/EU7x__9U0AYYu9K.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EU7x__9U0AYYu9K.jpg",
+					"url": "https://t.co/zt6DrDVUVu",
+					"display_url": "pic.twitter.com/zt6DrDVUVu",
+					"expanded_url": "https://twitter.com/LoneEden12/status/1247200406662340615/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 846,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1443,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1247200228630909000,
+					"id_str": "1247200228630908934",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/EU7x__9U0AYYu9K.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EU7x__9U0AYYu9K.jpg",
+					"url": "https://t.co/zt6DrDVUVu",
+					"display_url": "pic.twitter.com/zt6DrDVUVu",
+					"expanded_url": "https://twitter.com/LoneEden12/status/1247200406662340615/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 846,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1443,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1247200260851503000,
+					"id_str": "1247200260851503109",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/EU7yB3_UEAUVv0o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EU7yB3_UEAUVv0o.jpg",
+					"url": "https://t.co/zt6DrDVUVu",
+					"display_url": "pic.twitter.com/zt6DrDVUVu",
+					"expanded_url": "https://twitter.com/LoneEden12/status/1247200406662340615/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 472,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 833,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1422,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1181778014729728000,
+			"id_str": "1181778014729728000",
+			"name": "LONE EDEN",
+			"screen_name": "LoneEden12",
+			"location": "❌secondary process❌no credit❌",
+			"description": "론 에덴 (Lone Eden) for #이달의소녀 LOONA #올리비아혜 Olivia 🐺 + #고원 Gowon 🦋",
+			"url": "https://t.co/IM8bPspCUh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IM8bPspCUh",
+							"expanded_url": "http://instagram.com/loneeden12",
+							"display_url": "instagram.com/loneeden12",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7686,
+			"friends_count": 2,
+			"listed_count": 724,
+			"created_at": "Wed Oct 09 03:46:59 +0000 2019",
+			"favourites_count": 138,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 362,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1245184137214005248/m-qETu1U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1245184137214005248/m-qETu1U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1181778014729728000/1585710009",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 795,
+		"favorite_count": 2164,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "191126 ✨\n\n#고원 #Gowon #이달의소녀 #LOONA https://t.co/zt6DrDVUVu"
+	},
+	{
 		"created_at": "Sun Apr 05 20:28:49 +0000 2020",
 		"id": 1246459493409411000,
 		"id_str": "1246897604350193664",
