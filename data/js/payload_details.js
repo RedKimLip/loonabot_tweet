569a11a6820759f4fa183b6b4b2833622dc82050
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-04-20 00:04:24 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5067,
-	"created_at": "Sun Apr 19 07:04:23 +0000 2020",
+	"tweets": 5068,
+	"created_at": "Sun Apr 19 15:04:23 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sun Apr 19 14:50:24 +0000 2020",
+		"id": 1251824250740072400,
+		"id_str": "1251885867691458562",
+		"full_text": "191124 ICN\n#이달의소녀 #올리비아혜 #LOONA #OliviaHye https://t.co/15UOy0LCcc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						11,
+						17
+					]
+				},
+				{
+					"text": "올리비아혜",
+					"indices": [
+						18,
+						24
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						25,
+						31
+					]
+				},
+				{
+					"text": "OliviaHye",
+					"indices": [
+						32,
+						42
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1251823010635378700,
+					"id_str": "1251823010635378688",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/EV9eZU6U8AAs18S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EV9eZU6U8AAs18S.jpg",
+					"url": "https://t.co/15UOy0LCcc",
+					"display_url": "pic.twitter.com/15UOy0LCcc",
+					"expanded_url": "https://twitter.com/offbeat1113/status/1251824250740072448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1251823010635378700,
+					"id_str": "1251823010635378688",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/EV9eZU6U8AAs18S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EV9eZU6U8AAs18S.jpg",
+					"url": "https://t.co/15UOy0LCcc",
+					"display_url": "pic.twitter.com/15UOy0LCcc",
+					"expanded_url": "https://twitter.com/offbeat1113/status/1251824250740072448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1251823010643759000,
+					"id_str": "1251823010643759105",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/EV9eZU8U0AE3jCv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EV9eZU8U0AE3jCv.jpg",
+					"url": "https://t.co/15UOy0LCcc",
+					"display_url": "pic.twitter.com/15UOy0LCcc",
+					"expanded_url": "https://twitter.com/offbeat1113/status/1251824250740072448/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1074780731077910500,
+			"id_str": "1074780731077910528",
+			"name": "offbeat",
+			"screen_name": "offbeat1113",
+			"location": "",
+			"description": "늑깅이🖤 #이달의소녀 #올리비아혜",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15070,
+			"friends_count": 2,
+			"listed_count": 1383,
+			"created_at": "Mon Dec 17 21:37:49 +0000 2018",
+			"favourites_count": 104,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 437,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1230143119967977472/a3wHy5tt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1230143119967977472/a3wHy5tt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1074780731077910528/1582121769",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 887,
+		"favorite_count": 1858,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "191124 ICN\n#이달의소녀 #올리비아혜 #LOONA #OliviaHye https://t.co/15UOy0LCcc"
+	},
+	{
 		"created_at": "Sun Apr 19 06:27:42 +0000 2020",
 		"id": 1251499789683847200,
 		"id_str": "1251759357424885760",
