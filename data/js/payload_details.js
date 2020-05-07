5a8a0d188b18fea56fdf81c0510c86db75042f7e
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-05-07 09:04:24 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5088,
-	"created_at": "Wed Apr 29 06:04:24 +0000 2020",
+	"tweets": 5089,
+	"created_at": "Thu May 07 00:04:23 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_05.js
@@ -0,0 +1,229 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Wed May 06 23:06:35 +0000 2020",
+		"id": 1256223469672464400,
+		"id_str": "1258171332086206464",
+		"full_text": "🦉190224 미니팬미팅\n보고싶구나...\n\n#이달의소녀 #LOONA #김립 #KimLip https://t.co/cMCJFbJ0AD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			49
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						24,
+						30
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						31,
+						37
+					]
+				},
+				{
+					"text": "김립",
+					"indices": [
+						38,
+						41
+					]
+				},
+				{
+					"text": "KimLip",
+					"indices": [
+						42,
+						49
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1256223106689048600,
+					"id_str": "1256223106689048581",
+					"indices": [
+						50,
+						73
+					],
+					"media_url": "http://pbs.twimg.com/media/EW8AQoDWoAUWB1n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW8AQoDWoAUWB1n.jpg",
+					"url": "https://t.co/cMCJFbJ0AD",
+					"display_url": "pic.twitter.com/cMCJFbJ0AD",
+					"expanded_url": "https://twitter.com/rosypit_kr/status/1256223469672464390/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256223106689048600,
+					"id_str": "1256223106689048581",
+					"indices": [
+						50,
+						73
+					],
+					"media_url": "http://pbs.twimg.com/media/EW8AQoDWoAUWB1n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW8AQoDWoAUWB1n.jpg",
+					"url": "https://t.co/cMCJFbJ0AD",
+					"display_url": "pic.twitter.com/cMCJFbJ0AD",
+					"expanded_url": "https://twitter.com/rosypit_kr/status/1256223469672464390/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256223248360054800,
+					"id_str": "1256223248360054784",
+					"indices": [
+						50,
+						73
+					],
+					"media_url": "http://pbs.twimg.com/media/EW8AY30WoAAD1Sy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW8AY30WoAAD1Sy.jpg",
+					"url": "https://t.co/cMCJFbJ0AD",
+					"display_url": "pic.twitter.com/cMCJFbJ0AD",
+					"expanded_url": "https://twitter.com/rosypit_kr/status/1256223469672464390/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1220029250230636500,
+			"id_str": "1220029250230636545",
+			"name": "𝐑𝐨𝐬𝐲 𝐩𝐢𝐭.",
+			"screen_name": "rosypit_kr",
+			"location": "1위 가수 팬페이지",
+			"description": "#이달의소녀 #김립 #올리비아혜",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2090,
+			"friends_count": 1,
+			"listed_count": 307,
+			"created_at": "Wed Jan 22 17:04:05 +0000 2020",
+			"favourites_count": 16,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 44,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1256543324988534785/E9VwIfiB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1256543324988534785/E9VwIfiB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220029250230636545/1581286903",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 493,
+		"favorite_count": 1393,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🦉190224 미니팬미팅\n보고싶구나...\n\n#이달의소녀 #LOONA #김립 #KimLip https://t.co/cMCJFbJ0AD"
+	}
+]
