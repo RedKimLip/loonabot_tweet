211867b75eaf3d3f8c149519f2332b071c686f6a
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-05-09 14:04:24 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5089,
-	"created_at": "Thu May 07 00:04:23 +0000 2020",
+	"tweets": 5090,
+	"created_at": "Sat May 09 05:04:23 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 09 04:56:51 +0000 2020",
+		"id": 1258951842081325000,
+		"id_str": "1258984254773489664",
+		"full_text": "뚜두뚜두가 이렇게 귀여운 춤이였는지요 https://t.co/WtTX9nknQg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1258951642503770000,
+					"id_str": "1258951642503770113",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1258951642503770113/pu/img/La23hLU9WIQQzQAq.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1258951642503770113/pu/img/La23hLU9WIQQzQAq.jpg",
+					"url": "https://t.co/WtTX9nknQg",
+					"display_url": "pic.twitter.com/WtTX9nknQg",
+					"expanded_url": "https://twitter.com/zmfflq1234/status/1258951842081325057/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1048,
+							"h": 588,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1048,
+							"h": 588,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1258951642503770000,
+					"id_str": "1258951642503770113",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1258951642503770113/pu/img/La23hLU9WIQQzQAq.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1258951642503770113/pu/img/La23hLU9WIQQzQAq.jpg",
+					"url": "https://t.co/WtTX9nknQg",
+					"display_url": "pic.twitter.com/WtTX9nknQg",
+					"expanded_url": "https://twitter.com/zmfflq1234/status/1258951842081325057/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1048,
+							"h": 588,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1048,
+							"h": 588,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							262,
+							147
+						],
+						"duration_millis": 4200,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1258951642503770113/pu/vid/480x270/BX6FwnvWwOBJ2DC_.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1258951642503770113/pu/vid/640x360/YAB02KGtmoWWWDtV.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1258951642503770113/pu/pl/7B0mN5eQOR8FTX7m.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1258951642503770113/pu/vid/1048x588/qPJyiObcC6eA20nI.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1180291928031514600,
+			"id_str": "1180291928031514624",
+			"name": "여자영상저장소",
+			"screen_name": "zmfflq1234",
+			"location": "여자만/모멘트에 정리",
+			"description": "제보 안 받아요/잘못된 점, 비판등은 페잉이나 멘션해주세요! / @zmfflq1234",
+			"url": "https://t.co/PkehCUrKYD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PkehCUrKYD",
+							"expanded_url": "https://peing.net/ko/zmfflq1234?event=0",
+							"display_url": "peing.net/ko/zmfflq1234?…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67372,
+			"friends_count": 1,
+			"listed_count": 159,
+			"created_at": "Sat Oct 05 01:21:50 +0000 2019",
+			"favourites_count": 27,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1180291928031514624/1580997391",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": true,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1283,
+		"favorite_count": 961,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "뚜두뚜두가 이렇게 귀여운 춤이였는지요 https://t.co/WtTX9nknQg"
+	},
+	{
 		"created_at": "Wed May 06 23:06:35 +0000 2020",
 		"id": 1256223469672464400,
 		"id_str": "1258171332086206464",
