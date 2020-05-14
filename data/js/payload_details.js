c04320bcdcfe8f735b7bc1d5a814697b74faec61
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-05-14 09:04:25 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5092,
-	"created_at": "Sat May 09 22:04:23 +0000 2020",
+	"tweets": 5093,
+	"created_at": "Thu May 14 00:04:24 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,195 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 13 23:26:28 +0000 2020",
+		"id": 1260615005336072200,
+		"id_str": "1260713048664244225",
+		"full_text": "190601 \n#희진 #이달의소녀 #Heejin #LOONA https://t.co/SSDQ3zxTiR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "희진",
+					"indices": [
+						8,
+						11
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						12,
+						18
+					]
+				},
+				{
+					"text": "Heejin",
+					"indices": [
+						19,
+						26
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						27,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1260614875341971500,
+					"id_str": "1260614875341971457",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EX6ajNNU4AEEaDs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX6ajNNU4AEEaDs.jpg",
+					"url": "https://t.co/SSDQ3zxTiR",
+					"display_url": "pic.twitter.com/SSDQ3zxTiR",
+					"expanded_url": "https://twitter.com/STHL1019/status/1260615005336072192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1260614875341971500,
+					"id_str": "1260614875341971457",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EX6ajNNU4AEEaDs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX6ajNNU4AEEaDs.jpg",
+					"url": "https://t.co/SSDQ3zxTiR",
+					"display_url": "pic.twitter.com/SSDQ3zxTiR",
+					"expanded_url": "https://twitter.com/STHL1019/status/1260615005336072192/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1033391237636608000,
+			"id_str": "1033391237636608000",
+			"name": "SOMETHING HIGHLIGHT ✨",
+			"screen_name": "STHL1019",
+			"location": "",
+			"description": "전희진 예쁘다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17430,
+			"friends_count": 4,
+			"listed_count": 1314,
+			"created_at": "Sat Aug 25 16:30:45 +0000 2018",
+			"favourites_count": 162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152794061071216640/AUrLInrR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152794061071216640/AUrLInrR_normal.jpg",
+			"profile_link_color": "ED0090",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 539,
+		"favorite_count": 1704,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "190601 \n#희진 #이달의소녀 #Heejin #LOONA https://t.co/SSDQ3zxTiR"
+	},
+	{
 		"created_at": "Sat May 09 21:20:09 +0000 2020",
 		"id": 1259148842013884400,
 		"id_str": "1259231709595230208",
