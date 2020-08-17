03ce7baba63da73d9a811222c81d375de35078d0
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-08-17 11:04:24 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5104,
-	"created_at": "Thu Jul 09 09:04:23 +0000 2020",
+	"tweets": 5105,
+	"created_at": "Mon Aug 17 02:04:23 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_08.js
@@ -0,0 +1,279 @@
+Grailbird.data.tweets_2020_08 = 
+[
+	{
+		"created_at": "Mon Aug 17 01:39:46 +0000 2020",
+		"id": 1294895002590445600,
+		"id_str": "1295173441566289920",
+		"full_text": "180607\n\n#이달의소녀 #올리비아혜\n#손혜주 #혜주\n#LOONA #OliviaHye https://t.co/pskIwrMSsP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						8,
+						14
+					]
+				},
+				{
+					"text": "올리비아혜",
+					"indices": [
+						15,
+						21
+					]
+				},
+				{
+					"text": "손혜주",
+					"indices": [
+						22,
+						26
+					]
+				},
+				{
+					"text": "혜주",
+					"indices": [
+						27,
+						30
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						31,
+						37
+					]
+				},
+				{
+					"text": "OliviaHye",
+					"indices": [
+						38,
+						48
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1294894981191094300,
+					"id_str": "1294894981191094277",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/EfhkH-9U0AU_LzC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfhkH-9U0AU_LzC.jpg",
+					"url": "https://t.co/pskIwrMSsP",
+					"display_url": "pic.twitter.com/pskIwrMSsP",
+					"expanded_url": "https://twitter.com/Hug_1113/status/1294895002590445569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1294894981191094300,
+					"id_str": "1294894981191094277",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/EfhkH-9U0AU_LzC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfhkH-9U0AU_LzC.jpg",
+					"url": "https://t.co/pskIwrMSsP",
+					"display_url": "pic.twitter.com/pskIwrMSsP",
+					"expanded_url": "https://twitter.com/Hug_1113/status/1294895002590445569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1294894981274955800,
+					"id_str": "1294894981274955777",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/EfhkH_RUcAEHwux.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfhkH_RUcAEHwux.jpg",
+					"url": "https://t.co/pskIwrMSsP",
+					"display_url": "pic.twitter.com/pskIwrMSsP",
+					"expanded_url": "https://twitter.com/Hug_1113/status/1294895002590445569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1294894982055080000,
+					"id_str": "1294894982055079936",
+					"indices": [
+						49,
+						72
+					],
+					"media_url": "http://pbs.twimg.com/media/EfhkICLUMAAggBq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfhkICLUMAAggBq.jpg",
+					"url": "https://t.co/pskIwrMSsP",
+					"display_url": "pic.twitter.com/pskIwrMSsP",
+					"expanded_url": "https://twitter.com/Hug_1113/status/1294895002590445569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1177580324932341800,
+			"id_str": "1177580324932341760",
+			"name": "𝐇𝐮𝐠.",
+			"screen_name": "Hug_1113",
+			"location": "HQ = ❤️",
+			"description": "#LOONA #이달의소녀 #올리비아혜",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2291,
+			"friends_count": 0,
+			"listed_count": 203,
+			"created_at": "Fri Sep 27 13:46:50 +0000 2019",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1262775861989609477/cqQ72Vy9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1262775861989609477/cqQ72Vy9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1177580324932341760/1589904586",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 403,
+		"favorite_count": 1165,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "180607\n\n#이달의소녀 #올리비아혜\n#손혜주 #혜주\n#LOONA #OliviaHye https://t.co/pskIwrMSsP"
+	}
+]
