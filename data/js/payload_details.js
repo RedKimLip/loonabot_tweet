e2db5f61fbf13818ba1638cccfddaedcc3fafa75
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-09-22 00:04:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5117,
-	"created_at": "Mon Sep 21 10:04:25 +0000 2020",
+	"tweets": 5118,
+	"created_at": "Mon Sep 21 15:04:24 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 21 14:05:58 +0000 2020",
+		"id": 1308038488843890700,
+		"id_str": "1308044803544961031",
+		"full_text": "#이달의소녀 달의 주파수 오늘의 일일 DJ 현디니디와 함께한 치명적인 목소리의 주인공은 #김립 #진솔 #최리 였습니다☀️ 오빛과 함께 HOT한 여름보다 더 뜨거운 시간을 보낼 수 있어서 즐거웠어요❤️💙💜\n\n#LOONA #KimLip #JinSoul #Choerry https://t.co/szjrZRfDGx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "김립",
+					"indices": [
+						49,
+						52
+					]
+				},
+				{
+					"text": "진솔",
+					"indices": [
+						53,
+						56
+					]
+				},
+				{
+					"text": "최리",
+					"indices": [
+						57,
+						60
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						115,
+						121
+					]
+				},
+				{
+					"text": "KimLip",
+					"indices": [
+						122,
+						129
+					]
+				},
+				{
+					"text": "JinSoul",
+					"indices": [
+						130,
+						138
+					]
+				},
+				{
+					"text": "Choerry",
+					"indices": [
+						139,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1308038475308871700,
+					"id_str": "1308038475308871681",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EicWECFUMAEDYqg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EicWECFUMAEDYqg.jpg",
+					"url": "https://t.co/szjrZRfDGx",
+					"display_url": "pic.twitter.com/szjrZRfDGx",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1308038488843890690/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1538,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 901,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1308038475308871700,
+					"id_str": "1308038475308871681",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/EicWECFUMAEDYqg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EicWECFUMAEDYqg.jpg",
+					"url": "https://t.co/szjrZRfDGx",
+					"display_url": "pic.twitter.com/szjrZRfDGx",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1308038488843890690/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1538,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 901,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 533020,
+			"friends_count": 0,
+			"listed_count": 5588,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2159,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9435,
+		"favorite_count": 27471,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 달의 주파수 오늘의 일일 DJ 현디니디와 함께한 치명적인 목소리의 주인공은 #김립 #진솔 #최리 였습니다☀️ 오빛과 함께 HOT한 여름보다 더 뜨거운 시간을 보낼 수 있어서 즐거웠어요❤️💙💜\n\n#LOONA #KimLip #JinSoul #Choerry https://t.co/szjrZRfDGx"
+	},
+	{
 		"created_at": "Mon Sep 21 09:05:57 +0000 2020",
 		"id": 1307968020485034000,
 		"id_str": "1307969305217204225",
