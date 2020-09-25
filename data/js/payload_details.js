e251bb2301966388650e61fde58b8f06f63b6df2
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-09-26 01:04:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5129,
-	"created_at": "Fri Sep 25 04:04:24 +0000 2020",
+	"tweets": 5130,
+	"created_at": "Fri Sep 25 16:04:25 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Fri Sep 25 15:06:03 +0000 2020",
+		"id": 1309508020171206700,
+		"id_str": "1309509478430257153",
+		"full_text": "#이달의소녀 공식 팬클럽 'Orbit 2.0' 활동 기간과 관련하여 자세한 안내사항이 공식 팬카페를 통해 공개되었습니다💫 아래 표기된 주소로 접속하여 공지사항 게시판에서 공지문을 확인해 주세요!\n\n▶️ https://t.co/XKuLryfjHE\n⠀\n#LOONA https://t.co/f6JmPGCaQL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						139,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XKuLryfjHE",
+					"expanded_url": "http://cafe.daum.net/loonatheworld",
+					"display_url": "cafe.daum.net/loonatheworld",
+					"indices": [
+						113,
+						136
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1309508005298282500,
+					"id_str": "1309508005298282497",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/media/EixOl75VoAEyLQL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EixOl75VoAEyLQL.jpg",
+					"url": "https://t.co/f6JmPGCaQL",
+					"display_url": "pic.twitter.com/f6JmPGCaQL",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1309508020171206656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1309508005298282500,
+					"id_str": "1309508005298282497",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/media/EixOl75VoAEyLQL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EixOl75VoAEyLQL.jpg",
+					"url": "https://t.co/f6JmPGCaQL",
+					"display_url": "pic.twitter.com/f6JmPGCaQL",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1309508020171206656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 538896,
+			"friends_count": 0,
+			"listed_count": 5665,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2172,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3471,
+		"favorite_count": 14715,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 공식 팬클럽 'Orbit 2.0' 활동 기간과 관련하여 자세한 안내사항이 공식 팬카페를 통해 공개되었습니다💫 아래 표기된 주소로 접속하여 공지사항 게시판에서 공지문을 확인해 주세요!\n\n▶️ https://t.co/XKuLryfjHE\n⠀\n#LOONA https://t.co/f6JmPGCaQL"
+	},
+	{
 		"created_at": "Fri Sep 25 03:05:58 +0000 2020",
 		"id": 1309327010448117800,
 		"id_str": "1309328262699642882",
