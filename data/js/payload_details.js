eb1069098caf101d986b022158a85505b0cacc31
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-09-29 23:04:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5139,
-	"created_at": "Tue Sep 29 13:04:25 +0000 2020",
+	"tweets": 5140,
+	"created_at": "Tue Sep 29 14:04:24 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 29 13:05:58 +0000 2020",
+		"id": 1310927379552821200,
+		"id_str": "1310928807323488256",
+		"full_text": "잠시 후 밤 11시(KST) 이달의 소녀 LOOΠΔ On Wave [LOOΠΔTHEWORLD : Midnight Festival]의 상세 페이지 오픈 및 티켓 판매가 시작되오니 아래 표기된 주소로 접속하여 확인해 주세요!💫\n\n▶️ https://t.co/Nl01roKNmZ\n⠀\n#이달의소녀 #LOONA #Midnight https://t.co/FHb8uN4QWt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						155,
+						161
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						162,
+						168
+					]
+				},
+				{
+					"text": "Midnight",
+					"indices": [
+						169,
+						178
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Nl01roKNmZ",
+					"expanded_url": "https://mmt.fans/izHJ",
+					"display_url": "mmt.fans/izHJ",
+					"indices": [
+						129,
+						152
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1310926856619663400,
+					"id_str": "1310926856619663361",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/EjFZB89VoAERy9E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjFZB89VoAERy9E.jpg",
+					"url": "https://t.co/FHb8uN4QWt",
+					"display_url": "pic.twitter.com/FHb8uN4QWt",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1310927379552821248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1310926856619663400,
+					"id_str": "1310926856619663361",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/EjFZB89VoAERy9E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjFZB89VoAERy9E.jpg",
+					"url": "https://t.co/FHb8uN4QWt",
+					"display_url": "pic.twitter.com/FHb8uN4QWt",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1310927379552821248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 541971,
+			"friends_count": 0,
+			"listed_count": 5671,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2184,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5473,
+		"favorite_count": 15916,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "잠시 후 밤 11시(KST) 이달의 소녀 LOOΠΔ On Wave [LOOΠΔTHEWORLD : Midnight Festival]의 상세 페이지 오픈 및 티켓 판매가 시작되오니 아래 표기된 주소로 접속하여 확인해 주세요!💫\n\n▶️ https://t.co/Nl01roKNmZ\n⠀\n#이달의소녀 #LOONA #Midnight https://t.co/FHb8uN4QWt"
+	},
+	{
 		"created_at": "Tue Sep 29 12:05:58 +0000 2020",
 		"id": 1310912260559134700,
 		"id_str": "1310913708915339265",
