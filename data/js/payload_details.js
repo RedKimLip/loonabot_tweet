a637911ad49aa260ddea22ae94abef7f05974cfb
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-10-13 13:04:27 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5185,
-	"created_at": "Mon Oct 12 11:04:25 +0000 2020",
+	"tweets": 5186,
+	"created_at": "Tue Oct 13 04:04:26 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,10 +1,244 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Tue Oct 13 03:05:58 +0000 2020",
+		"id": 1315849744774762500,
+		"id_str": "1315851243793928192",
+		"full_text": "[이달의소녀탐구 #637]\n#이달의소녀탐구 #loonatv #이달의소녀 #LOONA https://t.co/3LfDPNUznL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀탐구",
+					"indices": [
+						15,
+						23
+					]
+				},
+				{
+					"text": "loonatv",
+					"indices": [
+						24,
+						32
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						33,
+						39
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						40,
+						46
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1315757259624857600,
+					"id_str": "1315757259624857600",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1315757259624857600/pu/img/XGkt1qcWY-JqGiDz.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1315757259624857600/pu/img/XGkt1qcWY-JqGiDz.jpg",
+					"url": "https://t.co/3LfDPNUznL",
+					"display_url": "pic.twitter.com/3LfDPNUznL",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1315849744774762496/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315757259624857600,
+					"id_str": "1315757259624857600",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1315757259624857600/pu/img/XGkt1qcWY-JqGiDz.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1315757259624857600/pu/img/XGkt1qcWY-JqGiDz.jpg",
+					"url": "https://t.co/3LfDPNUznL",
+					"display_url": "pic.twitter.com/3LfDPNUznL",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1315849744774762496/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 60494,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1315757259624857600/pu/pl/A-4n8SyrI_hCaN94.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1315757259624857600/pu/vid/1280x720/OOtN4sS0aOlefLXZ.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1315757259624857600/pu/vid/480x270/FYvjRa2sTOlAKmSN.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1315757259624857600/pu/vid/640x360/q4VFF38aCsuQrwjE.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 552650,
+			"friends_count": 0,
+			"listed_count": 5723,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2241,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2644,
+		"favorite_count": 8698,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[이달의소녀탐구 #637]\n#이달의소녀탐구 #loonatv #이달의소녀 #LOONA https://t.co/3LfDPNUznL"
+	},
+	{
 		"created_at": "Mon Oct 12 10:05:58 +0000 2020",
 		"id": 1315578299251187700,
 		"id_str": "1315594551571238913",
-		"full_text": "🌚 팬의점 달 상담소 사연 대모집 🌝\n⠀⠀⠀⠀\n달아, 달아 내 이야기를 들어줘!\n이 ‘달’의 소녀들에게 사연을 말해봐! \n\n🏃‍♀️🏃‍♂️상단 프로필에서 사연 신청하러 가기🏃‍♀️🏃‍♂️\nhttps://t.co/qeBLsLRCum https://t.co/ea4V0E8bYZ",
+		"full_text": "🌚 팬의점 달 상담소 사연 대모집 🌝\n⠀⠀⠀⠀\n달아, 달아 내 이야기를 들어줘!\n이 ‘달’의 소녀들에게 사연을 말해봐! \n\n🏃♀️🏃♂️상단 프로필에서 사연 신청하러 가기🏃♀️🏃♂️\nhttps://t.co/qeBLsLRCum https://t.co/ea4V0E8bYZ",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -182,7 +416,7 @@ Grailbird.data.tweets_2020_10 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "🌚 팬의점 달 상담소 사연 대모집 🌝\n⠀⠀⠀⠀\n달아, 달아 내 이야기를 들어줘!\n이 ‘달’의 소녀들에게 사연을 말해봐! \n\n🏃‍♀️🏃‍♂️상단 프로필에서 사연 신청하러 가기🏃‍♀️🏃‍♂️\nhttps://t.co/qeBLsLRCum https://t.co/ea4V0E8bYZ"
+		"text": "🌚 팬의점 달 상담소 사연 대모집 🌝\n⠀⠀⠀⠀\n달아, 달아 내 이야기를 들어줘!\n이 ‘달’의 소녀들에게 사연을 말해봐! \n\n🏃♀️🏃♂️상단 프로필에서 사연 신청하러 가기🏃♀️🏃♂️\nhttps://t.co/qeBLsLRCum https://t.co/ea4V0E8bYZ"
 	},
 	{
 		"created_at": "Mon Oct 12 08:05:58 +0000 2020",
