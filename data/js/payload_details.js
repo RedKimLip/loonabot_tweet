cfa888e55958b3b6708087b944fec212221c327e
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-10-20 00:04:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5222,
-	"created_at": "Mon Oct 19 12:04:25 +0000 2020",
+	"tweets": 5223,
+	"created_at": "Mon Oct 19 15:04:25 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 19 14:05:57 +0000 2020",
+		"id": 1318182698947522600,
+		"id_str": "1318191663269949441",
+		"full_text": "드디어 오빛과 야광봉을 흔들 수 있는 날이 왔어요! 잠시 후 오전 12시(KST) #이달의소녀 LOOΠΔ On Wave [LOOΠΔTHEWORLD : Midnight Festival]에서 오늘의 응원봉 컬러 흰색 라이트를 켜 주세요🤍 앞으로 랜선출첵 이벤트는 @loonastaff 에서 계속됩니다! 다같이 운동화 벗고 소리 질러~🌟👟\n⠀\n#LOONA https://t.co/PJD1PN5BJN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						46,
+						52
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						189,
+						195
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonaStaff",
+					"name": "이달의 소녀 Staff",
+					"id": 773078392660856800,
+					"id_str": "773078392660856832",
+					"indices": [
+						145,
+						156
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1318182210055204900,
+					"id_str": "1318182210055204866",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EksfvGuUcAIPG7B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EksfvGuUcAIPG7B.jpg",
+					"url": "https://t.co/PJD1PN5BJN",
+					"display_url": "pic.twitter.com/PJD1PN5BJN",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1318182698947522560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1318182210055204900,
+					"id_str": "1318182210055204866",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EksfvGuUcAIPG7B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EksfvGuUcAIPG7B.jpg",
+					"url": "https://t.co/PJD1PN5BJN",
+					"display_url": "pic.twitter.com/PJD1PN5BJN",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1318182698947522560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 557320,
+			"friends_count": 0,
+			"listed_count": 5703,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2293,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11262,
+		"favorite_count": 28978,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "드디어 오빛과 야광봉을 흔들 수 있는 날이 왔어요! 잠시 후 오전 12시(KST) #이달의소녀 LOOΠΔ On Wave [LOOΠΔTHEWORLD : Midnight Festival]에서 오늘의 응원봉 컬러 흰색 라이트를 켜 주세요🤍 앞으로 랜선출첵 이벤트는 @loonastaff 에서 계속됩니다! 다같이 운동화 벗고 소리 질러~🌟👟\n⠀\n#LOONA https://t.co/PJD1PN5BJN"
+	},
+	{
 		"created_at": "Mon Oct 19 11:05:58 +0000 2020",
 		"id": 1318009004145008600,
 		"id_str": "1318146364828778503",
