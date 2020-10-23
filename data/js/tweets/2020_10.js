2c6c57e6d4df959a423b68a06c37e9156344d30d
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-10-23 16:04:26 +0900

    Added 1 tweets

--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 23 06:05:57 +0000 2020",
+		"id": 1319518952452546600,
+		"id_str": "1319520418584297473",
+		"full_text": "V LIVE FANSHIP 이달의 소녀+(LOONA+)가 오픈되었습니다😆 앞으로 이달의 소녀+(LOONA+)를 통해 더 다양한 #이달의소녀 의 콘텐츠를 만나 보세요💕\n\n▶️ https://t.co/VGjjVo8Ocy\n\n#LOONA https://t.co/4ZMxZ7sOoO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						71,
+						77
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						122,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VGjjVo8Ocy",
+					"expanded_url": "https://www.vlive.tv/fanship/986CD9",
+					"display_url": "vlive.tv/fanship/986CD9",
+					"indices": [
+						97,
+						120
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1319515354108813300,
+					"id_str": "1319515354108813315",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/Ek_cOTVUYAM37M3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ek_cOTVUYAM37M3.jpg",
+					"url": "https://t.co/4ZMxZ7sOoO",
+					"display_url": "pic.twitter.com/4ZMxZ7sOoO",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1319518952452546561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1319515354108813300,
+					"id_str": "1319515354108813315",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/Ek_cOTVUYAM37M3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ek_cOTVUYAM37M3.jpg",
+					"url": "https://t.co/4ZMxZ7sOoO",
+					"display_url": "pic.twitter.com/4ZMxZ7sOoO",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1319518952452546561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 562467,
+			"friends_count": 0,
+			"listed_count": 5763,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2320,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3532,
+		"favorite_count": 12321,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "V LIVE FANSHIP 이달의 소녀+(LOONA+)가 오픈되었습니다😆 앞으로 이달의 소녀+(LOONA+)를 통해 더 다양한 #이달의소녀 의 콘텐츠를 만나 보세요💕\n\n▶️ https://t.co/VGjjVo8Ocy\n\n#LOONA https://t.co/4ZMxZ7sOoO"
+	},
+	{
 		"created_at": "Fri Oct 23 04:05:58 +0000 2020",
 		"id": 1319481367940395000,
 		"id_str": "1319490220220362757",
