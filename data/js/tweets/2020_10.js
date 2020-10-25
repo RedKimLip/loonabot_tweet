74ed1c3e6c5e727bc1028bf02ced68c8657da31b
Red Kim Lip <39099605+RedKimLip@users.noreply.github.com>
2020-10-25 22:04:27 +0900

    Added 1 tweets

--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sun Oct 25 12:06:04 +0000 2020",
+		"id": 1320329814016811000,
+		"id_str": "1320335817873723393",
+		"full_text": "#LOONA 's first week is wrapping up soon🙌 Have you gotten your [12:00] albums yet? We hope orbits like all 4 versions🥰 If you don't have the albums yet, you can still order your [12:00] albums! Enjoy the rest of your week, orbits💫\n\n#이달의소녀 #Midnight #Why_Not",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			257
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						232,
+						238
+					]
+				},
+				{
+					"text": "Midnight",
+					"indices": [
+						239,
+						248
+					]
+				},
+				{
+					"text": "Why_Not",
+					"indices": [
+						249,
+						257
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1320329618117726200,
+		"in_reply_to_status_id_str": "1320329618117726208",
+		"in_reply_to_user_id": 770959712791597000,
+		"in_reply_to_user_id_str": "770959712791597056",
+		"in_reply_to_screen_name": "loonatheworld",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 564092,
+			"friends_count": 0,
+			"listed_count": 5781,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2333,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 531,
+		"favorite_count": 4102,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "#LOONA 's first week is wrapping up soon🙌 Have you gotten your [12:00] albums yet? We hope orbits like all 4 versions🥰 If you don't have the albums yet, you can still order your [12:00] albums! Enjoy the rest of your week, orbits💫\n\n#이달의소녀 #Midnight #Why_Not"
+	},
+	{
 		"created_at": "Sun Oct 25 06:05:58 +0000 2020",
 		"id": 1320243696416116700,
 		"id_str": "1320245194990751744",
